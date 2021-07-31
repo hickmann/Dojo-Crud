90c91669fefeaa8874b3dd043f3ffd7f6aee3aa3
Rafael Hickmann <rafael.hickmann@datum.inf.br>
2021-07-31 10:36:38 -0300

    feat: Including Springboot initializr docs

---
+++ src/main/java/ch/hickmann/springbootfromzerotohero/SpringbootfromzerotoheroApplication.java
@@ -0,0 +1,13 @@
+package ch.hickmann.springbootfromzerotohero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootfromzerotoheroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootfromzerotoheroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/hickmann/springbootfromzerotohero/SpringbootfromzerotoheroApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.hickmann.springbootfromzerotohero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootfromzerotoheroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
